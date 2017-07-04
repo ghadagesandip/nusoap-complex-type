aca8f3b7aa5e1bd9a3fccf3d2140d93f59482321
Sandip Ghadge <sandip.ghadge@wwindia.com>
2017-07-04 21:06:13 +0530

    added query

--- functions.php
+++ functions.php
@@ -1,16 +1,40 @@
 <?php
 function getProductDetails($productId){
 
-    $mysqli = new mysqli("localhost", "root", "webwerks", "trainingapp");
+    $link = new mysqli("localhost", "root", "webwerks", "trainingapp");
 
-    if ($mysqli->connect_errno) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
+    if ($link->connect_errno) {
+        printf("Connect failed: %s\n", $link->connect_error);
         exit();
     }
 
+    $responseArr = array();
 
 
+    $query = "SELECT * FROM `products` LIMIT 5";
 
+    if ($result = mysqli_query($link, $query)) {
+
+        /* fetch associative array */
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            array_push($responseArr,
+                array(
+                    'id' => $row["id"],
+                    'name' =>$row["name"]
+                )
+
+            );
+        }
+
+        /* free result set */
+        mysqli_free_result($result);
+    }
+
+     print_r($responseArr); exit;
+
+//    $resource->free();
+//    $link->close();
 
 
     $result[0] = array(
